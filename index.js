c443e641c04eb591399885e8420b46136b83634b
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-06-27 11:04:21 +0545

    inserte into the database

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 const express = require('express');
 const mogoose = require('mongoose');
+
+const Product = require('./models/product.model.js');
 const app = express	()
-// const products= require('')
 
 
 app.listen(3000, () => {
@@ -16,17 +17,42 @@ app.get('/', (req, res) => {
   
 })  // This is the route that will be called when the user visits the homepage
 
-app.post('/api/products', (req, res) => {
+app.get('/api/products', async(req, res) => 
+    {
+        try {
+            const products = await Product.find();
+            res.status(200).json({products});
+        } 
+        catch (error) {
+            res.status(500).json({message: error.message})    
+        }
+    }
+)
+
+app.post('/api/products', async(req, res) => {
 try {
     
-    res.send(req.body);
-    console/log(req.body);
+    const product = await Product.create(req.body);
+    res.status(200).json({product});
 } 
 catch (error) {
     res.status(500).json({message: error.message})    
 }
    // res.send('Product created');
 })  // This is the route that will be called when the user sends a POST request to /api/products
+
+app.get('/api/products/:id', async(req, res) => {
+
+    try {
+        const  {id} = req.params;
+         const product =await Product.findById(id);
+      
+        res.status(200).json({product});
+
+       }
+    catch (error) {
+        res.status(500).json({message: error.message})    
+    }})
 
 mogoose.connect("mongodb+srv://diwash:diwash123@diwash.yrbo6ty.mongodb.net/?retryWrites=true&w=majority&appName=Diwash")
 .then(() => {
--- models/product.model.js
+++ models/product.model.js
@@ -1,12 +1,49 @@
+// const mongoose = require('mongoose');
+
+// const Schema = mongoose.Schema;
+
+// const ProductSchema = newSchema(
+//     {
+//         name: {
+//             required: [true, "Name is required"],
+
+//         },
+//         //price: Number,
+//         quantity: { 
+//             type: Number,
+//             required: [true, "Quantity is required"]
+//         },
+//         price: {
+//             type: Number,
+//             required: [true, "Price is required"]
+//         },
+//         Image  :  {
+//             type: String,
+//             required: [false, "Image is required"]
+//         },
+// },
+
+//     {
+//         timestamps: true
+//     }
+        
+// );
+
+// const Product = mongoose.model("Product", ProductSchema);
+
+// module.exports = Product;
+
+
 const mongoose = require('mongoose');
 
-const ProductSchema = mongoose.Schema(
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema(
     {
         name: {
-            required: [true, "Name is required"],
-
+            type: String,
+            required: [true, "Name is required"]
         },
-        //price: Number,
         quantity: { 
             type: Number,
             required: [true, "Quantity is required"]
@@ -15,18 +52,17 @@ const ProductSchema = mongoose.Schema(
             type: Number,
             required: [true, "Price is required"]
         },
-        Image  :  {
+        Image: {
             type: String,
             required: [false, "Image is required"]
         },
-},
-
+    },
     {
         timestamps: true
     }
-        
 );
 
 const Product = mongoose.model("Product", ProductSchema);
 
 module.exports = Product;
+
